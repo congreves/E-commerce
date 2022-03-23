3b828e44631c812a7884b9d4eb0948f757a64ede
Sandy <sandy.congreve@cmeducations.se>
2022-03-23 13:19:56 +0100

    Added functionality in reducers.js and connected it to the contetx file

--- src/store/Contexts.jsx
+++ src/store/Contexts.jsx
@@ -1,17 +1,23 @@
-import { createContext } from "react";
+import React, { createContext, useContext, useEffect, useReducer } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { schoolProducts } from "../data";
+import { cartReducer } from "./Reducers";
 
 const Cart = createContext();
 const Context = ({ children }) => {
-  const dispatch = useDispatch();
+ /* const dispatchRedux = useDispatch();
   const SET_PRODUCTS = "SET_PRODUCTS";
+
+  const SELECTED_PRODUCTS = "SELECTED_PRODUCTS";
+  const SELECTED_PRODUCT = "SELECTED_PRODUCT";
+
   const setProducts = (products) => {
     return {
       type: SET_PRODUCTS,
       payload: products,
     };
   };
+
   const fetchProducts = () => async (dispatch) => {
     const response = await schoolProducts.get("/productlist");
     dispatch({
@@ -19,6 +25,7 @@ const Context = ({ children }) => {
       payload: response.data,
     });
   };
+
   const fetchProduct = (id) => async (dispatch) => {
     const response = await schoolProducts.get(`/product/${id}`);
     dispatch({
@@ -26,8 +33,14 @@ const Context = ({ children }) => {
       payload: response.data,
     });
   };
-  const products = useSelector((state) => state.allProducts.products);
-  const cartProducts = products.map((product) => ({
+
+  useEffect(() => {
+    dispatchRedux(fetchProducts());
+  }, []);
+  */
+
+  const schoolProducts = useSelector((state) => state.allProducts.products);
+  const cartProducts = schoolProducts.map((product) => ({
     id: product.id,
     title: product.title,
     price: product.price,
@@ -35,4 +48,19 @@ const Context = ({ children }) => {
     description: product.description,
     qty: product.qty,
   }));
+  const [state, dispatch] = useReducer(cartReducer, {
+    products: cartProducts,
+    cart: [],
+  });
+
+return (
+    <Cart.Provider value={{state, dispatch}}>
+        {children}
+    </Cart.Provider>
+    )
+    
 };
+export const CartState = () => {
+    return useContext(Cart);
+}
+export default Context;
